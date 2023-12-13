190ee6c5c7fae90bdfeca0769dfd05167d263a2e
rme2002 <romnick2005@gmail.com>
2023-12-13 19:24:31 +0100

    Now it cannot return a named entity that is in question

--- entity_linker/enity_picker.py
+++ entity_linker/enity_picker.py
@@ -23,7 +23,7 @@ def most_related_entity(question_entities, answer_entities):
             for q_entity in question_entities:
                 q_entity_page = getWikipedia(q_entity)
 
-                if q_entity_page is not None:
+                if q_entity_page is not None and a_entity != q_entity:
                     q_entity_ngrams = set(ngrams(q_entity_page[0]['dbpedia_page'].split('/'), 1))
                     a_entity_ngrams = set(ngrams(a_entity_page[0]['dbpedia_page'].split('/'), 1))
 
@@ -37,12 +37,12 @@ def most_related_entity(question_entities, answer_entities):
     return related_entity
 
 # Example usage
-q_txt = '''Who is the director of Inception?'''
-a_txt = '''Christopher Nolan hopefully not the same person who directed the dreadful 'The Prestige', a film that I found to be extremely disappointing.Why do you say "hopefully"? Nolan's career isn't going downhill or anything, so why hope he doesn't ruin something?Well for one thing, 'The Dark Knight' is one of the worst movies ever made, and 'Inception' has a lot to live up to. And I don't like the idea of Nolan doing a film set in the world of magic, because his approach to it will be so clinical, so technical...Well you have a point there. But then again, Nolan is an amazing visual director and has been pretty consistent with that side of things.I still haven't seen "The Dark Knight". I thought Batman Begins was cool, though, especially the Joker. But I don't like it when films try to be realistic and then have a magician character in them. It always seems so out of place...What about 'Harry Potter'?That series is full of magic, but there isn't really any focus on magic as an'''
+q_txt = '''Who painted 'Guernica'?'''
+a_txt = '''everybody knows. It was Picasso. What they don’t know, however, is that the work was a collaboration between many artists; or rather, it was an act of solidarity with Spain by many artists all over the world. It began as a reaction against the bombing of Gernika in 1937 and ended up as one of the most powerful images of war ever made.The story begins in 1936 when, under Franco, the Spanish Civil War broke out. On 26 April 1937, the Republican-held town of Guernica in northern Spain was bombed by German and Italian aircraft. The attack lasted for hours and destroyed everything in its path. It was a horrific war crime that shocked the world: 100 people were killed immediately and an estimated 500 injured; 80% of the town was reduced to rubble.The bombing took place during the Spanish Civil War, which broke out in July 1936 after a failed coup led by army generals and right-wing politicians against the democratically elected government. The war pitted Franco’s Nationalists – who were supported by Naz'''
 q_clean_txt = q_txt.translate(str.maketrans(' ', ' ', string.punctuation))
 a_clean_txt = a_txt.translate(str.maketrans(' ', ' ', string.punctuation))
 result = most_related_entity(getNamedEntity(q_clean_txt)[0], getNamedEntity(a_clean_txt)[0])
 print("Most related entity:", result)
 
-print(q_clean_txt)
+print(getNamedEntity(a_txt)[0])
 
